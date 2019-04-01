be2b969cb581b0b31dc298a8d4acf2a3b8d76089
Abhishek Gupta <cruderock@gmail.com>
2019-04-01 02:26:44 -0700

    Fixed Login. #U9

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -47,7 +47,7 @@ export default {
         })
         .then(response => {
           // this.resolve = response.data;
-          self.axios.defaults.headers.common["token"] = response.data;
+          //self.axios.defaults.headers.common["token"] = response.data;
           // this.resolve;
           self.$router.push({ name: "user" });
         });
