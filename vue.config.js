6eff8cee5111235f5868eea3c6f29e2963345a20
Abhishek Gupta <cruderock@gmail.com>
2019-04-02 17:38:26 -0700

    FIXES.

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
-module.exports = {
-    devServer: {
-        port: 8080,
-        https: true
-    },
-}
+// module.exports = {
+//     devServer: {
+//         port: 8080,
+//         https: true
+//     },
+// }
